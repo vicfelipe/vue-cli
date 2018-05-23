c1ec7cfa688098bbeefdf25149d19eedd4b341be
Victor Felipe Alencastro <vicfelipe@live.com>
2018-05-22 23:52:46 -0300

    Initial Files

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Firebase from './components/Firebase.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Firebase
+  },
+  data: {
+     dados: 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Firebase.vue
@@ -0,0 +1,26 @@
+<script>
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyB2hPY06r5oClBFi5btdtnvsiD6FY7phlE",
+    authDomain: "clact-99673.firebaseapp.com",
+    databaseURL: "https://clact-99673.firebaseio.com",
+    projectId: "clact-99673",
+    storageBucket: "clact-99673.appspot.com",
+    messagingSenderId: "148402098340"
+}
+var app = firebase.initializeApp(config);
+var db = app.database()
+var listRef = db.ref('palestrantes')
+
+export default {
+    name: 'Firebase',
+    firebase: {
+      listRef: listRef
+    }
+}
+</script>
+
+<style>
+
+</style>
